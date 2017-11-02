ec89b3dfda76688a55aa63ea42060e63dba2c4a4
motekito <motekito@gmail.com>
2017-11-02 09:11:57 +0300

    Блокнот

--- test_something.py
+++ test_something.py
@@ -3,8 +3,8 @@ import os.path
 project_dir = os.path.dirname(os.path.abspath(__file__))
 import sys
 
-sys.path.append(os.path.join(project_dir, "TestStack.White.9.2.0.11\\lib\\net40\\"))
-sys.path.append(os.path.join(project_dir, "Castle.Core.3.1.0\\lib\\net40-client\\"))
+sys.path.append(os.path.join(project_dir, "TestStack.White.0.13.3\\lib\\net40\\"))
+sys.path.append(os.path.join(project_dir, "Castle.Core.3.3.0\\lib\\net40-client\\"))
 clr.AddReferenceByName('TestStack.White')
 
 from TestStack.White import Application
